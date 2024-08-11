f4ddb81d987be529fe676490e02291081c6cfa3a
karnaukhov-o <karnaukhov.o@roistat.com>
2024-08-11 19:01:05 +0300

    catalog  v1

--- wp-content/themes/istobal/archive-catalog.php
+++ wp-content/themes/istobal/archive-catalog.php
@@ -1,53 +1,97 @@
 <?php get_header(); ?>
-<div class="breadcrumbs first">
-    <?php
-    if (function_exists('bcn_display')) {
-        bcn_display();
-    }
-    ?>
-</div>
-MENU:<br>
-<?php
-
-$taxonomy = 'product_cat'; // Укажите название вашей таксономии, например, 'category' для категорий записей
-$categories = get_terms(array(
-    'taxonomy' => $taxonomy,
-    'hide_empty' => false, // Установите в true, если вы хотите скрыть пустые категории
-));
-
-if (!empty($categories) && !is_wp_error($categories)) {
-    foreach ($categories as $category) {
-        if ($category->parent == 0) {
-            echo '<a href="' . get_term_link($category) . '">' . $category->name . '</a><br>';
-        }
-    }
-}
-
-$args = array(
-    'post_type' => 'catalog',
-);
-
-$query = new WP_Query($args);
-
-if ($query->have_posts()) {
-    echo "This is all posts:<br>"; ?>
-    <div class="catalog-list">
+<div class="catalog container">
+    <div class="breadcrumbs first">
         <?php
-        while ($query->have_posts()) {
-            $query->the_post();
-            $desc = get_field('desc'); ?>
-            <div class="catalog-item">
-                <img src="<?= get_the_post_thumbnail_url() ?>" alt="">
-                <a href="<?= get_permalink() ?>"><?= get_the_title() ?></a>
-                <span class="desc"><?= $desc ?></span>
+        if (function_exists('bcn_display')) {
+            bcn_display();
+        }
+        ?>
+    </div>
+    <div class="category-view">
+        <div class="category-image"><img class="image"
+                                         src="/wp-content/themes/istobal/assets/img/Automatic_car_wash_ISTOBAL_1.jpg"
+                                         alt=""></div>
+        <div class="page-title-wrapper">
+            <h1 class="page-title" id="page-title-heading" aria-labelledby="page-title-heading toolbar-amount">
+                <span class="base" data-ui-id="page-title-wrapper"><span
+                            class="first-word"> Car </span> wash machines</span></h1>
+        </div>
+        <div class="category-description">
+            <div data-content-type="row" data-appearance="contained" data-element="main">
+                <div data-enable-parallax="0" data-parallax-speed="0.5" data-background-images="{}" data-element="inner"
+                     style="justify-content: flex-start; display: flex; flex-direction: column;
+                     background-position: left top; bottom: ;ackground-size: cover; background-repeat: no-repeat;
+                     background-attachment: scroll; border-style: none; border-width: 1px; border-radius: 0px;
+                     margin: 0px 0px 10px; padding: 10px;">
+                    <div data-content-type="html" data-appearance="default" data-element="main"
+                         style="border-style: none; border-width: 1px; border-radius: 0px; margin: 0px; padding: 0px;">
+                        <p><span style="font-size: large;">Get to know the wide range of ISTOBAL vehicle washes for all types of vehicles. State-of-the-art solutions for &nbsp;automatic car wash and tunnels or jet wash centres. Maximum technology and reliability to increase the profitability of your car wash business.</span>
+                        </p></div>
+                </div>
             </div>
-        <?php } ?>
-    </div> <?php
-} else {
-    echo "no posts";
-}
+        </div>
+    </div>
+    <div class="catalog-body">
+        <div class="sidebar">
+            <nav class="left-menu">
+                <div class="menu_title">MENU</div>
+                <ul>
+                    <?
+                    $taxonomy = 'product_cat'; // Укажите название вашей таксономии, например, 'category' для категорий записей
+                    $categories = get_terms(array(
+                        'taxonomy' => $taxonomy,
+                        'hide_empty' => false, // Установите в true, если вы хотите скрыть пустые категории
+                    ));
 
-wp_reset_postdata();
-get_footer();
-?>
+                    if (!empty($categories) && !is_wp_error($categories)) {
+                        foreach ($categories as $category) {
+                            if ($category->parent == 0) {
+                                echo '<li><a href="' . get_term_link($category) . '">' . $category->name . '</a></li>';
+                            }
+                        }
+                    }
+                    ?>
+                </ul>
+            </nav>
+        </div>
+        <?
+        $args = array(
+            'post_type' => 'catalog',
+        );
+
+        $query = new WP_Query($args);
+        ?>
+        <div class="catalog-wrapper">
+            <div class="toolbar">
+                <div class="count"><strong><?= $query->found_posts ?></strong> items</div>
+            </div>
+            <?php
+
+
+            if ($query->have_posts()) { ?>
+                <div class="catalog-list">
+                    <?php
+                    while ($query->have_posts()) {
+                        $query->the_post();
+                        $desc = get_field('desc'); ?>
+                        <div class="catalog-item">
+                            <a href="<?= get_permalink() ?>" class="image">
+                                <img src="<?= get_the_post_thumbnail_url() ?>" alt="">
+                            </a>
+                            <div class="catalog-item-info">
+                                <a href="<?= get_permalink() ?>" class="catalog-item-title"><?= get_the_title() ?></a>
+                                <div class="catalog-item-desc"><?= $desc ?></div>
+                            </div>
+                        </div>
+                    <?php } ?>
+                </div> <?php
+            } else {
+                echo "no posts";
+            }
+
+            wp_reset_postdata(); ?>
+        </div>
+    </div>
+</div>
+<? get_footer(); ?>
 
